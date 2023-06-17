9152dc52e3e22c7aa67bd14aa01d99357e044745
Monasef <monasef77@gmail.com>
2023-06-17 11:00:36 +0100

    modify comps

--- src/FlashCards.jsx
+++ src/FlashCards.jsx
@@ -1,25 +1,25 @@
 import { useState } from 'react';
 
 function FlashCards({ questions }) {
-  const [selectedId, setSelectedId] = useState(0);
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
 
   return (
     <div className="flashcards">
-      {questions.map((question) => {
-        const classNames = selectedId === question.id ? 'selected' : '';
-        return (
-          <div
-            className={classNames}
-            onClick={() => {
-              selectedId === question.id
-                ? setSelectedId(0)
-                : setSelectedId(question.id);
-            }}
-          >
-            {selectedId === question.id ? question.answer : question.question}
-          </div>
-        );
-      })}
+      {questions.map((question) => (
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? 'selected' : ''}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
+        </div>
+      ))}
     </div>
   );
 }
